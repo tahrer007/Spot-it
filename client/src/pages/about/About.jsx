5594dd4e326c455fa330ef9faa18016fa32c10b3
tahrer007 <tahrer_Ad@outlook.com>
2022-06-25 23:30:19 +0300

    change to dynamic text

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -8,44 +8,17 @@ const About = () => {
   return (
     <div className="pagesContainer about">
       <div className="homePageLeft aboutText">
-        <div className="textBox">
-          <h1>About me </h1>
-
-          <h3> Who am? </h3>
-          <p>
-            my name is Tahrer, I am a Full Stack Developer who believes in
-            making the world a better place by technology.
-          </p>
-          <h3> Where I am From?</h3>
-          <p>
-            I was born and raised in Jerusalem and currently reside in Haifa.
-          </p>
-        </div>
-        <div className=" textBox">
-          <h1>About the App </h1>
-
-          <h3> About the idea </h3>
-          <p>
-            The wild pigs of Haifa have invaded almost all the city. as a
-            stranger, I didn't know where I can be without meeting them.
-          </p>
-          <h3> How it is work ? </h3>
-          <p>
-            The app shows all the locations of spotted boars in the last 48
-            hours. as well as the most affected neighborhoods.
-          </p>
-        </div>
-
-        <div className=" textBox">
-          <h1> Next level </h1>
-          <h3> Short term targets? </h3>
-          <p>
-            To send notifications to subscribers and provide help in emergency
-            cases. (under development)
-          </p>
-          <h3> Long term targets? </h3>
-          <p>To collaborate with the authorities and go global.</p>
-        </div>
+        {aboutText.map((x) => (
+          <div className="textBox">
+            <h1>{x.boxTitle} </h1>
+            {x.questions.map((y) => (
+              <>
+                <h3> {y.question} </h3>
+                <p>{y.answer}</p>
+              </>
+            ))}
+          </div>
+        ))}
       </div>
 
       <div className="aboutRight">
