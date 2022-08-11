82b6d97962ea40a5b17fd09bae70ad30236e8060
tahrer007 <tahrer_Ad@outlook.com>
2022-08-11 19:58:21 +0300

    git checkout git checkout

---
+++ client/src/components/newLocationInput/submitButton/SubmitButton.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+
+export default function SubmitButton({ handelForm }) {
+  
+
+  return (
+    <div className="homeText form">
+     
+          <button type="submit" className="btn">Submit</button>
+          
+    </div>
+  );
+}
